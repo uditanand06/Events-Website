fe9ebd5d3e6904d808765e31234b6b6739fa2063
uditanand06 <uditanand84@gmail.com>
2021-05-15 19:43:28 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -8,9 +8,13 @@ const AboutUsPage = () => {
     const [showModal, setShowModal] = useState(false)
     return (
         <Layout title='About DJ Events'>
-            <h1>About</h1>
-            <p>This is an app to find the latest DJ and other musical events</p>
-            <p>Version: 1.0.0</p>
+            <div className='h-[75vh]'>
+                <h1>About</h1>
+                <p>This is an app to find the latest DJ and other musical events</p>
+                <p>Version: 1.0.0</p>
+
+            </div>
+            
       </Layout>
     )
 }
